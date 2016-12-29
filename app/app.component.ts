b0bb06e6f183dd20833f230641bf4ca0980d5fde
Russell Williams <russellwilliamscc@gmail.com>
2016-12-29 23:11:03 +0000

    adds basic form to screen

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,39 @@
 import { Component } from '@angular/core';
+import { User }    from './user';
 
 @Component({
+  moduleId: module.id,  
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<input type="text" id="firstName"
+             [(ngModel)]="model.firstName" name="firstName" placeholder="First Name" required>
+      <input type="text" id="lastName"
+             [(ngModel)]="model.lastName" name="lastName" placeholder="Last name" required>
+      <input type="text" id="email"
+             [(ngModel)]="model.email" name="email" placeholder="E-mail" required>
+      <input type="text" id="phone"
+             [(ngModel)]="model.phone" name="phone" placeholder="Phone" required>
+
+      <select id="country" [(ngModel)]="model.country" name="country" required>
+        <option value="" disabled>Country</option>
+        <option value="uk">United Kingdom</option>
+        <option value="europe">Europe</option>
+        <option value="usa">USA</option>
+        <option value="china">China</option>
+      </select>
+      <input type="text" id="city"
+             [(ngModel)]="model.city" name="city" placeholder="City" required>
+      <input type="text" id="postcode"
+             [(ngModel)]="model.postCode" name="postCode" placeholder="Post code" required>
+      <input type="text" id="address"
+            [(ngModel)]="model.address" name="address" placeholder="Address" required>
+      <input type="text" id="additional"
+             [(ngModel)]="model.additionalInfo" name="additionalInfo" placeholder="Additional info">
+      <button type="submit" id="submit" >Submit</button>
+
+             `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+
+  model = new User('','','','','','','','','');
+
+ }
---
+++ app/user.ts
@@ -0,0 +1,15 @@
+export class User {
+
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+    public phone: string,
+    public country: string,
+    public city: string,
+    public postCode: string,
+    public address: string,
+    public additionalInfo: string
+  ) {  }
+
+}
